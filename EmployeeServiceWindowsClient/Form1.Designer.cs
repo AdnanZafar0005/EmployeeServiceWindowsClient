38d8799bbfb1a03153ebfffc9f5d3ffff2206956
Adnan Zafar <adnanzaffar0005@outlook.com>
2021-10-07 09:57:13 +0500

    Add project files.

---
+++ EmployeeServiceWindowsClient/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace EmployeeServiceWindowsClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxDOB = new System.Windows.Forms.TextBox();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Date of Birth";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(206, 38);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(121, 20);
+            this.textBoxId.TabIndex = 4;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(206, 71);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(121, 20);
+            this.textBoxName.TabIndex = 5;
+            // 
+            // textBoxDOB
+            // 
+            this.textBoxDOB.Location = new System.Drawing.Point(206, 136);
+            this.textBoxDOB.Name = "textBoxDOB";
+            this.textBoxDOB.Size = new System.Drawing.Size(122, 20);
+            this.textBoxDOB.TabIndex = 7;
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBoxGender.Location = new System.Drawing.Point(206, 112);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxGender.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Get Employee";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(222, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Save Employee";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(86, 206);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 11;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(382, 176);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 12;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 406);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxGender);
+            this.Controls.Add(this.textBoxDOB);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Employees";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxDOB;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ EmployeeServiceWindowsClient/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeServiceWindowsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EmployeeService.EmployeeServiceClient client = new  EmployeeService.EmployeeServiceClient();
+            if (textBoxId.Text.ToString() != "")
+            {
+                EmployeeService.Employee employee = client.GetEmployee(Convert.ToInt32(textBoxId.Text));
+             //   var typeemp = typeof(employee.);
+                if(employee == null)
+                {
+                    lblMessage.ForeColor = Color.Red;
+                    textBoxName.Text = textBoxId.Text = textBoxDOB.Text = comboBoxGender.Text = "";
+
+                    lblMessage.Text = "Employee does not found.";
+                    return;
+                }
+              
+
+                textBoxName.Text = employee.Name;
+
+                comboBoxGender.Text = employee.Gender;
+                textBoxDOB.Text = employee.DateOfBirth.ToShortDateString();
+                lblMessage.ForeColor = Color.Green;
+                lblMessage.Text = "Employee retrieved";
+            }
+            else
+            {
+                lblMessage.ForeColor = Color.Red;
+                lblMessage.Text = "Invalid Id. Please enter correct id.";
+            }
+            
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EmployeeService.Employee employee = new EmployeeService.Employee();
+
+
+
+            if (textBoxId.Text != "" && textBoxName.Text != "" && textBoxDOB.Text != "" && comboBoxGender.Text != "")
+            {
+
+                employee.Id = Convert.ToInt32(textBoxId.Text);
+                employee.Name = textBoxName.Text;
+                employee.Gender = comboBoxGender.Text;
+                employee.DateOfBirth = Convert.ToDateTime(textBoxDOB.Text);
+                 EmployeeService.EmployeeServiceClient client = new EmployeeService.EmployeeServiceClient();
+                client.SaveEmployee(employee);
+                lblMessage.ForeColor = Color.Green;
+                lblMessage.Text = "Employee saved";
+                
+            }
+            else
+            {
+                lblMessage.ForeColor = Color.Red;
+                lblMessage.Text = "Please check your all fields are corect.";
+               
+            }
+           
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            textBoxDOB.Text = textBoxId.Text = textBoxName.Text = comboBoxGender.Text = lblMessage.Text = "";
+            
+
+        }
+    }
+}
